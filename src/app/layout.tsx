309589fb724a539d83707638c7a3683811c19dc8
Nerevarine22 <taras.bratash@gmail.com>
2026-08-03 21:56:44 +0300

    fix: disable user scaling and double-tap zoom on mobile devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,8 @@ const spaceGrotesk = Space_Grotesk({
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export const metadata: Metadata = {
